24c521570d7f1b62877c2ed73ddb79a761c476b1
Jose Carlos <jcfb90@gmail.com>
2018-08-14 13:19:15 -0500

    change

---
+++ src/components/UserCard/UserCardList.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import UserCard from "./UserCard";
+
+class UserCardList extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default UserCardList;
