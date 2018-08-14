5d67fabbd856629a46a43039d4de38cc1920a3cc
Jose Carlos <jcfb90@gmail.com>
2018-08-14 14:09:44 -0500

    Change for home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
 import Top from "../components/Top/Top";
 import Footer from "../components/Footer/Footer";
+import config from "../services/config";
+
+const API_HASH = "6de6abfedb24f889e0b5f675edc50deb";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataList: []
+    };
+  }
+
+  dataFetch = () => {
+    fetch(config.defaultURL + API_HASH)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          dataList: result
+        });
+      });
+  };
+
+  componentDidMount() {
+    this.dataFetch();
+    console.log(this.state.dataList);
+  }
+
   render() {
     return (
       <div>
         <Top />
         <div className="container">
+          <p> Los datos desde el fetch </p>
           <Footer />
         </div>
       </div>
